4f58aee1523898f4431fa1cbf254fb4d590c51d7
Justin Lafferty <justin.lafferty@kw.com>
2024-02-03 19:43:08 -0500

    render window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+use bevy::window::{PresentMode, WindowTheme};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Snake".into(),
+                resolution: (800., 600.).into(),
+                present_mode: PresentMode::AutoVsync,
+                window_theme: Some(WindowTheme::Dark),
+                enabled_buttons: bevy::window::EnabledButtons {
+                    maximize: false,
+                    ..Default::default()
+                },
+                visible: true,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
