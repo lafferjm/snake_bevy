f08751ce11f8d4e964b334900abdf0252e044453
Justin Lafferty <justin.lafferty@kw.com>
2024-02-03 20:11:47 -0500

    snake moves

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@ use bevy::prelude::*;
 use bevy::window::{PresentMode, PrimaryWindow, WindowTheme};
 
 #[derive(Component)]
-pub struct Snake {}
+struct Snake {}
 
-pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+#[derive(Component, PartialEq)]
+enum Direction {
+    NONE,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
+
+fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
     commands.spawn(Camera2dBundle {
         transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.),
@@ -12,7 +21,7 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     });
 }
 
-pub fn spawn_snake(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+fn spawn_snake(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
 
     commands.spawn((
@@ -26,7 +35,50 @@ pub fn spawn_snake(mut commands: Commands, window_query: Query<&Window, With<Pri
             ..default()
         },
         Snake {},
+        Direction::NONE,
     ));
+}
+
+fn move_snake(mut transform_query: Query<&mut Transform, With<Snake>>, direction_query: Query<&Direction, With<Snake>>) {
+    let direction = direction_query.get_single().unwrap();
+
+    if let Ok(mut transform) = transform_query.get_single_mut() {
+        if *direction == Direction::UP {
+            transform.translation += Vec3::new(0., 20., 0.);
+        }
+
+        if *direction == Direction::DOWN {
+            transform.translation += Vec3::new(0., -20., 0.);
+        }
+
+        if *direction == Direction::LEFT {
+            transform.translation += Vec3::new(-20., 0., 0.);
+        }
+
+        if *direction == Direction::RIGHT {
+            transform.translation += Vec3::new(20., 0., 0.);
+        }
+    }
+}
+
+fn handle_input(keyboard_input: Res<Input<KeyCode>>, mut snake_query: Query<&mut Direction, With<Snake>>) {
+    if let Ok(mut direction) = snake_query.get_single_mut() {
+        if keyboard_input.just_pressed(KeyCode::Left) && *direction != Direction::RIGHT {
+            *direction = Direction::LEFT;
+        }
+
+        if keyboard_input.just_pressed(KeyCode::Right) && *direction != Direction::LEFT {
+            *direction = Direction::RIGHT;
+        }
+
+        if keyboard_input.just_pressed(KeyCode::Up) && *direction != Direction::DOWN {
+            *direction = Direction::UP;
+        }
+
+        if keyboard_input.just_pressed(KeyCode::Down) && *direction != Direction::UP {
+            *direction = Direction::DOWN;
+        }
+    }
 }
 
 fn main() {
@@ -47,7 +99,7 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, spawn_camera)
-        .add_systems(Update, spawn_snake)
+        .add_systems(Startup, (spawn_camera, spawn_snake))
+        .add_systems(Update, (move_snake, handle_input))
         .run();
 }
